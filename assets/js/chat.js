abddd7d8bdfb2de7700a60e2f41acffcf830c7bc
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2019-12-11 00:20:23 +0000

    refactored chat js to take room address as init param

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,27 +1,29 @@
-$(() => {
-    let socket = null;
-
-    $("#chatbox").submit(() => {
-        const msgBox = $("#chatbox textarea");
-
-        if (!msgBox.val()) return false;
-        if (!socket) {
-            alert("Error: There is no socket connection.");
+function initChat(roomAddress) {
+    $(() => {
+        let socket = null;
+    
+        $("#chatbox").submit(() => {
+            const msgBox = $("#chatbox textarea");
+    
+            if (!msgBox.val()) return false;
+            if (!socket) {
+                alert("Error: There is no socket connection.");
+                return false;
+            }
+    
+            socket.send(msgBox.val());
+            msgBox.val("");
+            
+            return false;
+        });
+    
+        if (!window["WebSocket"]) {
+            alert("Error: Your browser does not support web sockets.")
             return false;
         }
-
-        socket.send(msgBox.val());
-        msgBox.val("");
-        
-        return false;
+    
+        socket = new WebSocket(roomAddress);
+        socket.onclose = () => alert("Connection has been closed.");
+        socket.onmessage = (e) => $("#messages").append($("<li>").text(e.data));
     });
-
-    if (!window["WebSocket"]) {
-        alert("Error: Your browser does not support web sockets.")
-        return false;
-    }
-
-    socket = new WebSocket("ws://{{.Host}}/room");
-    socket.onclose = () => alert("Connection has been closed.");
-    socket.onmessage = (e) => $("#messages").append($("<li>").text(e.data));
-});
+}
